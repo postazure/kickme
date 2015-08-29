b5d4bbd2fc80234bb002fefb4a9365ad67799478
Max Gordon <gordonmaxc@gmail.com>
2015-08-28 22:28:21 -0700

    Whitespace

--- app/controllers/project_creators_controller.rb
+++ app/controllers/project_creators_controller.rb
@@ -11,13 +11,12 @@ class ProjectCreatorsController < ApplicationController
 
   def create
     project_creator = ProjectCreator.create(project_creator_params)
-
     render :index
   end
 
   def search
     results = KickstarterApiClient.new
-                  .search_project_creators_by_name(params['search_name'])
+      .search_project_creators_by_name(params['search_name'])
     render json: results
   end
 
